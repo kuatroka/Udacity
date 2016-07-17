ea8014899bd94b0ce751c01a89353076c581b246
kuatroka <aaarmani_2000@yahoo.com>
2016-07-17 18:32:55 +0800

    ...

--- CodeCa_battleship.py
+++ CodeCa_battleship.py
@@ -2,7 +2,7 @@ from random import randint
 
 board = []
 
-for x in range(0, 5):
+for x in range(5):
     board.append(["O"] * 5)
 
 
@@ -11,19 +11,40 @@ def print_board(board):
         print " ".join(row)
 
 
-# Add your code below!
+print "Let's play Battleship!"
+print_board(board)
+
+
 def random_row(board):
     return randint(0, len(board) - 1)
 
 
 def random_col(board):
-    return randint(0, len(board) - 1)
+    return randint(0, len(board[0]) - 1)
 
 
-guess_row = int(raw_input("Guess Row: "))
-guess_col = int(raw_input("Guess Column: "))
+ship_row = random_row(board)
+ship_col = random_col(board)
+print ship_row
+print ship_col
 
-if guess_row == ship_row and guess_col == ship_col:
-    print "Congratulations! You sank my battleship!"
+# Everything from here on should go in your for loop!
+# Be sure to indent four spaces!
+for turns in range(4):
+    print "Turn", turns + 1
+    guess_row = int(raw_input("Guess Row: "))
+    guess_col = int(raw_input("Guess Col: "))
 
+    if guess_row == ship_row and guess_col == ship_col:
+        print "Congratulations! You sunk my battleship!"
+    else:
+        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+            print "Oops, that's not even in the ocean."
+        elif (board[guess_row][guess_col] == "X"):
+            print "You guessed that one already."
+        else:
+            print "You missed my battleship!"
 
+    board[guess_row][guess_col] = "X"
+
+print_board(board)
